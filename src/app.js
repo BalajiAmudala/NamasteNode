4369ddc7463a05ffe3bece7cbca39da6335d56e7
balajiamudala <yogibalu18@gmail.com>
2025-12-23 05:42:24 +0530

    Exploring different routes in Express JS

--- src/app.js
+++ src/app.js
@@ -2,18 +2,41 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Namaste from Dashboard !!");
+app.get("/users/:userId/books/:bookId", (req, res) => {
+  console.log(req.params);
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello !!");
+app.get(/.*fly$/, (req, res) => {
+  res.send("/.*fly$/");
 });
 
-app.use("/test", (req, res) => {
-  res.send("test file changes!!");
+app.get("/flights/:from-:to", (req, res) => {
+  console.log(req.params);
+  res.send("Data received bro !!");
 });
 
+// app.post("/user", (req, res) => {
+//   res.send("Successfully posted the user data to DB !!!");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("Successfully deleted the user Data from DB !!!");
+// });
+
+// app.patch("/user", (req, res) => {
+//   res.send("Updated the user data successfully !");
+// });
+// app.use("/hello", (req, res) => {
+//   res.send("Hello hello hello !!");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("test file changes!!");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Namaste from Dashboard !!");
+// });
 app.listen(7777, () => {
   console.log("server is successfully listening on port 7777....");
 });
