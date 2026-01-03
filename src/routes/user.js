a3af1c2976eefb4732486af27f6c73f4c583aacd
balajiamudala <yogibalu18@gmail.com>
2026-01-03 16:10:57 +0530

    pagination code

--- src/routes/user.js
+++ src/routes/user.js
@@ -56,7 +56,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     //no cards of connection request send already
 
     const loggedInUser = req.user;
-
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
     //find all connection requests (sent + received)
     const connectionRequest = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser }, { toUserId: loggedInUser }],
@@ -67,14 +70,16 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       hideUsersFromFeed.add(req.fromUserId.toString());
       hideUsersFromFeed.add(req.toUserId.toString());
     });
-    console.log(hideUsersFromFeed);
 
     const users = await User.find({
       $and: [
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.send(users);
   } catch (err) {
