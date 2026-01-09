80d009cfdee423d435971ebd94786ec4bb08652a
balajiamudala <yogibalu18@gmail.com>
2026-01-09 22:40:56 +0530

    some fixes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://dhanvi_user11:x73BMY24ZQG5REHK@cluster0.jwmu0kg.mongodb.net/devTinder"
+    "mongodb+srv://dhanvi_user11:GvUkzCWmQgM6AVww@cluster0.jwmu0kg.mongodb.net/devTinder"
   );
 };
 
